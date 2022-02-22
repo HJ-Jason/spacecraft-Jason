7b8768842a676e7cffdaf7a8a827d58a2b0dfbb5
HJ-Jason <jason.heldebaume03@gmail.com>
2022-02-22 12:52:58 +0100

    ESLint Prettier

--- App.tsx
+++ App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { LoginScreen } from "./src/screens/LoginScreen";
-import { TermsScreen } from "./src/screens/TermsScreen";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { StarshipFeedScreen } from "./src/screens/StarshipFeedScreen";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     //<LoginScreen />
     //<TermsScreen />
-    //<StarshipFeedScreen />
-    <QueryClientProvider client={queryClient}>...</QueryClientProvider>
 
+    <QueryClientProvider client={queryClient}>
+      <StarshipFeedScreen />
+    </QueryClientProvider>
   );
 };
 
---
+++ cheatcodes.md
@@ -0,0 +1,34 @@
+# CHEATCODES FOR AWESOMENESS
+
+## tsrnfs + tab
+
+    import { StyleSheet, Text, View } from 'react-native'
+    import React from 'react'
+
+    type Props = {}
+
+    const LoginScreen = (props: Props) => {
+    return (
+        <View>
+        <Text>LoginScreen</Text>
+        </View>
+    )
+    }
+
+    export default LoginScreen
+
+    const styles = StyleSheet.create({})
+
+## tsrafce + tab
+
+    import React from 'react'
+
+    type Props = {}
+
+    const LoginScreen = (props: Props) => {
+    return (
+        <div>LoginScreen</div>
+    )
+    }
+
+    export default LoginScreen
